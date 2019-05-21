a436369e52fa0bc5c5024b857e408ed549a7b776
Epdomon Ghooms <ghooms@epdomain>
2019-05-20 20:45:26 -0400

    added jupyter notebook and Rachels cleaning of transmitted data

---
+++ prc/aprs/reformat_aprs.py
@@ -0,0 +1,36 @@
+
+# W.A. Watters, 2019.05.20
+# This script converts the UTC time to seconds since epoch in EDT
+# also converts nan speed values to 0.0
+
+import time
+import calendar
+import csv
+import numpy as np
+import epdobase as eb
+
+ep = eb.cl_epdobase()
+ep.load('./aprs_raw_copy.csv','csv')
+ep.numerize()
+
+epout = eb.cl_epdobase()
+epout.init_fields(['time','lat','lng','speed','altitude'])
+
+times = []
+
+for i in range(ep.len()):
+    currtime = ep.dcty['time'][i]
+    timeinsec= calendar.timegm(time.strptime(currtime,'%Y-%m-%d %H:%M:%S'))
+    if np.isnan(ep.ndct['speed'][i]):
+        ep.ndct['speed'][i] = 0.0
+    times.append(timeinsec+4*3600)
+
+for fld in epout.flds:
+    epout.ndct[fld] = ep.ndct[fld]
+    
+epout.ndct['time'] = np.array(times)
+epout.characterize()
+
+epout.save('csv','aprs_reformated.csv')
+
+
